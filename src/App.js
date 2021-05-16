b27d853f56399bcc2db46cb27b37ae76d4b0f343
petnwil <peterwilhelmsen@Peters-MacBook-Pro.local>
2021-05-16 22:38:21 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Nasjonaldagen</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Velkommen til Nasjonaldagen i Brunholmgata 1C!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
